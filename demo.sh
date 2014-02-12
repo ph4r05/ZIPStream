3738d19beb1bb78895dada8decfa769585826e22
ph4r05 <ph4r05@gmail.com>
2014-02-12 17:22:10 +0100

    simple demo shell script

---
+++ demo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cat bbc_weather.apk | CLASSPATH=target/ZIPStream-1.0-SNAPSHOT.jar java cz.muni.fi.xklinec.zipstream.Mallory -f 2 --padd-extra 8192  --cmd '/bin/cp <<INPUTAPK>> <<OUTPUTAPK>>' > haha.apk
+
